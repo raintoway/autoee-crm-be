51ecc030503890f921ca4be6826f52341cd7566d
caojiafu@cvte.com <caojiafu@cvte.com>
2025-05-27 23:32:38 +0800

    infra

---
+++ yudao-module-infra/yudao-module-infra-biz/target/generated-sources/annotations/cn/iocoder/yudao/module/infra/convert/file/FileConfigConvertImpl.java
@@ -0,0 +1,29 @@
+package cn.iocoder.yudao.module.infra.convert.file;
+
+import cn.iocoder.yudao.module.infra.controller.admin.file.vo.config.FileConfigSaveReqVO;
+import cn.iocoder.yudao.module.infra.dal.dataobject.file.FileConfigDO;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-21T19:00:27+0800",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 1.8.0_432 (Tencent)"
+)
+public class FileConfigConvertImpl implements FileConfigConvert {
+
+    @Override
+    public FileConfigDO convert(FileConfigSaveReqVO bean) {
+        if ( bean == null ) {
+            return null;
+        }
+
+        FileConfigDO.FileConfigDOBuilder fileConfigDO = FileConfigDO.builder();
+
+        fileConfigDO.id( bean.getId() );
+        fileConfigDO.name( bean.getName() );
+        fileConfigDO.storage( bean.getStorage() );
+        fileConfigDO.remark( bean.getRemark() );
+
+        return fileConfigDO.build();
+    }
+}
---
+++ yudao-module-infra/yudao-module-infra-biz/target/generated-sources/annotations/cn/iocoder/yudao/module/infra/convert/redis/RedisConvertImpl.java
@@ -0,0 +1,11 @@
+package cn.iocoder.yudao.module.infra.convert.redis;
+
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-21T19:00:27+0800",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 1.8.0_432 (Tencent)"
+)
+public class RedisConvertImpl implements RedisConvert {
+}
