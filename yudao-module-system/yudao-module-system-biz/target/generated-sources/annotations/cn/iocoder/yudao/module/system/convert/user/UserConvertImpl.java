8b0253fa7e292d17f8ea01eb5a354b079c3cad42
caojiafu@cvte.com <caojiafu@cvte.com>
2025-05-27 23:32:52 +0800

    system

---
+++ yudao-module-system/yudao-module-system-biz/target/generated-sources/annotations/cn/iocoder/yudao/module/system/convert/user/UserConvertImpl.java
@@ -0,0 +1,11 @@
+package cn.iocoder.yudao.module.system.convert.user;
+
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-21T18:58:00+0800",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 1.8.0_432 (Tencent)"
+)
+public class UserConvertImpl implements UserConvert {
+}
